25793622e8e7af98011fbab73ce7a8330d7eaecf
empolem33 <inspiron0925@yahoo.com>
2023-03-19 14:29:46 -0700

    solved

--- javascript/isShapeCube.js
+++ javascript/isShapeCube.js
@@ -2,3 +2,10 @@
 //return Return true if the cuboid could have equal sides, return false otherwise.
 //example 125, 5), true);
 //pusdo chech length equll to voulume
+
+var cubeChecker = function (volume, side) {
+  if (side < 1 || volume < 1) return false;
+  return volume == side ** 3;
+};
+
+console.log(cubeChecker(8, 2));
