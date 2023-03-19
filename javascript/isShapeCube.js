18859ad3b44e8a27ee7b2706a5ed879a42f79bae
empolem33 <inspiron0925@yahoo.com>
2023-03-19 14:20:43 -0700

    prep

---
+++ javascript/isShapeCube.js
@@ -0,0 +1,4 @@
+//params take in volume and length see if cube
+//return Return true if the cuboid could have equal sides, return false otherwise.
+//example 125, 5), true);
+//pusdo chech length equll to voulume
