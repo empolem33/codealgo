64efa7a2ebcfe9fc9cf09fe08b6ad872d97b7e9b
empolem33 <inspiron0925@yahoo.com>
2023-03-21 15:18:52 -0700

    pt!

---
+++ javascript/masteringbasics.js
@@ -0,0 +1,8 @@
+//Parametor: The function should take three arguments - operation(string/char), value1(number), value2(number).
+//Return: The function should return result of numbers after applying the chosen operation.
+//example: Examples(Operator, value1, value2) --> output
+//* ('+', 4, 7) --> 11
+//* ('-', 15, 18) --> -3
+//* ('*', 5, 5) --> 25
+//* ('/', 49, 7) --> 7
+//pseudo do switch case
