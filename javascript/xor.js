00b7c506a943c3dc9d291cd9f9e6d6dcc44df269
empolem33 <inspiron0925@yahoo.com>
2023-03-21 15:39:43 -0700

    xor solved

--- javascript/xor.js
+++ javascript/xor.js
@@ -2,3 +2,13 @@
 //return true if they are difrent values else false
 //example true false => true  || true true => false
 //pseudo compare vals return tre or false
+function xor(a, b) {
+  // TODO: Program Me
+  if (a != b) {
+    return true;
+  }
+  return false;
+}
+
+console.log(xor(true, true));
+console.log(xor(true, false));
