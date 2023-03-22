884014b23b1e206cd4cdeaf9a0893fa26d6edb8a
empolem33 <inspiron0925@yahoo.com>
2023-03-22 15:05:58 -0700

    solved

--- javascript/stringrepeat.js
+++ javascript/stringrepeat.js
@@ -2,3 +2,13 @@
 //return a string of s repeated exactly n times.
 //*example 6, "I"     -> "IIIIII" 5, "Hello" -> "HelloHelloHelloHelloHello"
 //pseudo loop n times and add string return total
+
+function repeatStr(n, s) {
+  let text = "";
+  for (let i = 0; i < n; i++) {
+    text += s;
+  }
+  return text;
+}
+
+console.log(repeatStr(5, "Hello"));
