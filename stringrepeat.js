eea9577c1c8b3f713e5c54c5d82ac6b94a20b8a9
empolem33 <inspiron0925@yahoo.com>
2023-03-22 14:59:25 -0700

    prep starts

---
+++ stringrepeat.js
@@ -0,0 +1,4 @@
+//*parametors Write a function that accepts an integer n and a string s as parameters,
+//return a string of s repeated exactly n times.
+//*example 6, "I"     -> "IIIIII" 5, "Hello" -> "HelloHelloHelloHelloHello"
+//pseudo loop n times and add string return total
