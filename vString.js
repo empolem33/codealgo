69d68851f77f3d3001f5aadb26624c4e3497ff98
empolem33 <inspiron0925@yahoo.com>
2023-03-24 14:57:56 -0700

    prep-1

---
+++ vString.js
@@ -0,0 +1,10 @@
+//prams take in text  and char
+//return text replaced by char
+//example text before = "abc"
+// character   = "z"
+// text after  = "zzz"
+//psudo map text
+
+function contamination(text, char) {
+  // Code here ;)
+}
