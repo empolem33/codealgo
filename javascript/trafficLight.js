55faa63905cb66e7c366a6e6827c1ed441b50321
empolem33 <inspiron0925@yahoo.com>
2023-03-25 18:33:08 -0700

    solved

--- javascript/trafficLight.js
+++ javascript/trafficLight.js
@@ -5,4 +5,19 @@
 
 function updateLight(current) {
   //your code here!
+  //   switch (current) {
+  //     case "green":
+  //       return "yellow";
+  //     case "yellow":
+  //       return "red";
+  //     case "red":
+  //       return "green";
+  //   }
+  return current === "yellow"
+    ? "red"
+    : current === "green"
+    ? "yellow"
+    : "green";
 }
+
+console.log(updateLight("blue"));
