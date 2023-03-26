34f3f4f6d40d3a00ba4125ad6ad78ead9a92ed69
empolem33 <inspiron0925@yahoo.com>
2023-03-25 18:38:02 -0700

    sbmi calc

---
+++ javascript/bmicalc.js
@@ -0,0 +1,12 @@
+//params take in weigh
+//return bmi status
+//example if bmi <= 18.5 return "Underweight"
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+//psusdo swhic case
+function bmi(weight, height) {
+  return "";
+}
