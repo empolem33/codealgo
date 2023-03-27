096ef28e91133fce7947f5b554314bf967d45562
empolem33 <inspiron0925@yahoo.com>
2023-03-26 19:06:27 -0700

    name prep solved

--- javascript/twoName.js
+++ javascript/twoName.js
@@ -2,3 +2,10 @@
 //return two capital letters with a dot separating them.
 //example Sam Harris => S.H
 //psudo take letters and capp
+function abbrevName(name) {
+  const s = name.split(" ")[0].slice(0, 1);
+  const x = name.split(" ")[1].slice(0, 1);
+  return `${s.toUpperCase()}.${x.toUpperCase()}`;
+}
+
+console.log(abbrevName("sam harris"));
