ab44e5ce6e936bdc9fda685dbece650179896de2
empolem33 <inspiron0925@yahoo.com>
2023-03-27 19:58:34 -0700

    solved

--- javascript/ndivisivleY.js
+++ javascript/ndivisivleY.js
@@ -3,4 +3,6 @@
 //example n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
 //psudo divide and return
 
-function isDivisible(n, x, y) {}
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0;
+}
