692090783d6db1b1f30a868645849b4224451c31
empolem33 <inspiron0925@yahoo.com>
2023-03-27 19:55:06 -0700

    prep

---
+++ javascript/ndivisivleY.js
@@ -0,0 +1,6 @@
+//params take a num and divide by x ,y
+//return check if divisibl
+//example n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+//psudo divide and return
+
+function isDivisible(n, x, y) {}
